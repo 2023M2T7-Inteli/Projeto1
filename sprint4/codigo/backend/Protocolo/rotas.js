2ed597e5e17759228d5a79d4e8e38dd71e8a7336
yLogiiks <enzo.bressane@sou.inteli.edu.br>
2023-06-08 00:24:08 -0300

    comentários no código e WAD atualizado
    WAD por kakau, ariel e kai

--- sprint4/codigo/backend/Protocolo/rotas.js
+++ sprint4/codigo/backend/Protocolo/rotas.js
@@ -1,10 +1,14 @@
+// Importing necessary modules: Router function from express module and the query function from the query module.
 import { Router } from "express";
+// Import a custom utility function for running SQL queries.
 import query from "../utils/query.js";
+
+// A Router instance is created and assigned to the variable protocoloRouter.
 const protocoloRouter = Router();
 
 // Read all records from the Protocolo table
 protocoloRouter.get('/', async (req, res) => {
-	// Set response headers
+	 // The HTTP status code is set to 200 and cross-origin requests are allowed.
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	// Query to select all records from the Protocolo table
@@ -20,22 +24,21 @@ protocoloRouter.post('/insereProtocolo', async (req, res) => {
 	// Set response headers
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	// Open the database connection
 	// SQL query to insert a new record into the Protocolo table
 	const sql = "INSERT INTO Protocolo (nome, status) VALUES ('" + req.body.nome + "', '" + req.body.status + "')";
 	// Execute the query to insert the new record
 	const response = await query(sql);
-	// End the response
+	// Redirect to the new form page
     res.redirect("/html/newForm.html")
 });
 
 // Retrieve a specific record for updating
-protocoloRouter.get('/atualizarProtocolo', async (req, res) => {
+protocoloRouter.post('/atualizarProtocoloGet', async (req, res) => {
 	// Set response headers
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	// SQL query to select a specific record from the Protocolo table
-	const sql = "SELECT * FROM Protocolo WHERE ID_Protocolo=" + req.query.ID_Protocolo;
+	const sql = "SELECT * FROM Protocolo WHERE ID_Protocolo=" + req.body.ID_Protocolo;
 	// Execute the query and return the result as JSON
 	const response = await query(sql);
 	
@@ -56,16 +59,18 @@ protocoloRouter.post('/atualizarProtocolo', async (req, res) => {
 });
 
 // Delete a record from the Protocolo table
-protocoloRouter.get('/removeProtocolo', async (req, res) => {
+protocoloRouter.post('/removeProtocolo', async (req, res) => {
 	// Set response headers
 	res.statusCode = 200;
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	// SQL query to delete a record from the Protocolo table
-	const sql = "DELETE FROM Protocolo WHERE ID_Protocolo='" + req.query.ID_Protocolo + "'";
+	const sql = "DELETE FROM Protocolo WHERE ID_Protocolo='" + req.body.ID_Protocolo + "'";
 	// Execute the query to delete the record
 	const response = await query(sql);
 	
+	// The response is sent as JSON.
     res.json(response)
 });
 
+// protocoloRouter is exported for use in other parts of the application.
 export default protocoloRouter;
--- sprint4/codigo/backend/dbConnection.js
+++ sprint4/codigo/backend/dbConnection.js
@@ -1,9 +1,13 @@
+// Import necessary modules.
 import { fileURLToPath } from "url";
 import path from "path";
 
+// Define __filename and __dirname for ES modules.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Define the path to the database file.
 const DBPATH = path.join(__dirname, "..", "data", "banco.db"); 
 
+// Export the database path for use in other modules.
 export default DBPATH;
